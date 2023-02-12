db671e355ddf7084343bc6ed4c608690817cd55f
bilgeyillmaz <96742976+bilgeyillmaz@users.noreply.github.com>
2023-02-12 16:44:59 +0300

    Add project files.

---
+++ WebServiceApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebServiceApplication
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Please write a text:");
+            string text = Console.ReadLine();
+            Console.WriteLine("Calling WebAPI...");
+            HttpClient client = new HttpClient();
+            string apiUrl = "https://api.funtranslations.com/translate/yoda.json?text=" + text;
+            var response = await client.GetAsync(apiUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Message from web API: " + message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
